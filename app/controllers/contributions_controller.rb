604cf48620dc23cfe8da1d11df61ab36c1116907
Khwilo Kabaka <kwhyzzy@gmail.com>
2017-09-30 00:17:53 +0300

    Set up the contributions

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -5,4 +5,15 @@ class ContributionsController < ApplicationController
   def new
     @contribution = Contribution.new
   end
+
+  def create
+    @contribution = Contribution.create(contribution_params)
+    redirect_to contributions_path
+  end
+
+  private
+  
+  def contribution_params
+    params.require(:contribution).permit(:phone, :description, :amount, :image)
+  end
 end
